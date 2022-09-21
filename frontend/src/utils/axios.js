831f261deb0eff51befa1b4755ce7fa5714ce5eb
Artem Kasheev <comrade19632@gmail.com>
2022-09-21 16:37:44 +0300

    fix get new tokens

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,6 +3,7 @@ import { setAxiosAuthToken } from 'utils/Utils'
 
 const baseURL = 'https://apiproxy.telphin.ru/api/ver1.0/'
 const internalApiLoginUrl = (process.env.NODE_ENV === 'production') ? `${window.location.origin}/api/login/` : 'http://localhost/api/login/'
+const internalLoginUrl = (process.env.NODE_ENV === 'production') ? `${window.location.origin}/login` : 'http://localhost:3000/login'
 
 const instance = axios.create({
   baseURL,
@@ -28,9 +29,7 @@ instance.interceptors.response.use((response) => response, (error) => {
   const originalRequest = error.config
 
   // eslint-disable-next-line no-underscore-dangle
-  if (error.response.status === 401 && !originalRequest._retry && 
-    !(window.location.href === (process.env.NODE_ENV === 'production') ? `${window.location.origin}/login` : 'http://localhost:3000/login')) {
-
+  if (error.response.status === 401 && !originalRequest._retry && (window.location.href !== internalLoginUrl)) {
     if (isRefreshing) {
       return new Promise((resolve, reject) => {
         failedQueue.push({ resolve, reject })
